051828217e600150911ba793fafa24bf9f660334
yuki-fujisaki <yuki.f.bizz@gmail.com>
2022-08-12 23:10:32 +0900

    [done]feature/7a_sorted_by_favorite_created_time

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments
   has_many :favorites
+  has_many :favorited_users, through: :favorites, source: :user
   
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
