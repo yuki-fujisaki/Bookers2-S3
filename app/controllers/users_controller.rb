29bf0af5d6a53ce846dbb155f5ea1d2872b2b7be
yuki-fujisaki <yuki.f.bizz@gmail.com>
2022-08-16 22:49:56 +0900

    [done]feature/9b_async_search_result

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,18 @@ class UsersController < ApplicationController
     @book = Book.new
   end
 
+  def search
+    @user = User.find(params[:user_id])
+    @books = @user.books 
+    @book = Book.new
+    if params[:created_at] == ""
+      @search_book = "日付を選択してください"
+    else
+      create_at = params[:created_at]
+      @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count
+    end
+end
+
   def edit
   end
 
