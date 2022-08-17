cf6d93e6c881d15e5f48a20f7303209722711ada
yuki-fujisaki <yuki.f.bizz@gmail.com>
2022-08-17 13:07:48 +0900

    [WIP]feature/7c_create_models_controller_relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+  has_many :group_users
   
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   # フォロー・フォロワー機能修正箇所
